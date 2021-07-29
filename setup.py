8a48540829bb0ef149418079acde108c6612e4ff
ChillarAnand <anand21nanda@gmail.com>
2021-07-29 10:06:11 +0530

    chore: Remove ERPNext

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-with open('erpnext/__init__.py', 'rb') as f:
+with open('healthcare/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='erpnext',
+	name='erpnext_healthcare',
 	version=version,
-	description='Open Source ERP',
+	description='Open Source ERP Healthcare',
 	author='Frappe Technologies',
 	author_email='info@erpnext.com',
 	packages=find_packages(),
