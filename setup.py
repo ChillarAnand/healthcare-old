8f3f0eaf24f988268628ed5d7222585ee0dd192d
ChillarAnand <anand21nanda@gmail.com>
2021-08-03 08:11:54 +0530

    chore: Fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-with open('healthcare/__init__.py', 'rb') as f:
+with open('erpnext_healthcare/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
