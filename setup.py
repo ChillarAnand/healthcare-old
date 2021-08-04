592c7850d75dfc683129aacb90bb1617acfd1dd9
ChillarAnand <anand21nanda@gmail.com>
2021-08-04 21:12:02 +0550

    feat: Initialize App

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from setuptools import setup, find_packages
-import re, ast
 
-# get version from __version__ variable in erpnext/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-with open('healthcare/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+# get version from __version__ variable in healthcare/__init__.py
+from healthcare import __version__ as version
 
 setup(
-	name='healthcare',
+	name="healthcare",
 	version=version,
-	description='Open Source ERP Healthcare',
-	author='Frappe Technologies',
-	author_email='info@erpnext.com',
+	description="a",
+	author="a",
+	author_email="a",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
