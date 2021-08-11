0d28f1483e0494cf4f87618b64883f16fbd3ea2e
ChillarAnand <anand21nanda@gmail.com>
2021-08-11 15:23:24 +0530

    chore: Clean up install script

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -21,9 +21,13 @@ cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
+
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
+
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'172.18.0.1' IDENTIFIED BY 'test_frappe'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'172.18.0.1'"
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
@@ -36,17 +40,18 @@ sudo apt-get install libcups2-dev
 
 cd ~/frappe-bench || exit
 
-bench get-app https://github.com/chillarAnand/erpnext --branch chc
-bench get-app https://github.com/ChillarAnand/healthcare
 
 sed -i 's/watch:/# watch:/g' Procfile
 sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
+bench get-app https://github.com/chillarAnand/erpnext --branch chc
+
 bench start &> bench_run_logs.txt &
+bench --site test_site reinstall --yes
 
 bench --verbose --site test_site install-app erpnext
-bench --verbose --site test_site install-app healthcare
 
-bench --site test_site reinstall --yes
+bench get-app https://github.com/ChillarAnand/healthcare
+bench --verbose --site test_site install-app healthcare
